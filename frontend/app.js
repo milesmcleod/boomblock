311430b69c6ac205d6e94d8c49e72337d86d0719
Miles McLeod <milesmcleod123@gmail.com>
2017-12-05 11:49:27 -0800

    Add color-changing functionality to buttons

--- frontend/app.js
+++ frontend/app.js
@@ -25,20 +25,24 @@ document.addEventListener('DOMContentLoaded', () => {
       case 'play':
         if (!audio.playing) {
           audio.masterGain.gain.value = 1;
-          clickElement.object.material.color.set(0xffffff);
+          clickElement.object.material.color.set(0x000000);
           world.scene.children[3].children.filter(obj => (
             obj.name === 'pause'
-          ))[0].material.color.set(0x000000);
+          ))[0].material.color.set(0xffffff);
+          audio.start();
+          world.scene.children[3].children.filter(obj => (
+            obj.name === 'reset'
+          ))[0].material.color.set(0xffffff);
           audio.start();
         }
         break;
       case 'pause':
         if (audio.playing) {
           audio.masterGain.gain.value = 0;
-          clickElement.object.material.color.set(0xffffff);
+          clickElement.object.material.color.set(0x000000);
           world.scene.children[3].children.filter(obj => (
             obj.name === 'play'
-          ))[0].material.color.set(0x000000);
+          ))[0].material.color.set(0xffffff);
           audio.stop();
           window.removeEventListener('mouseup', handleClick, false);
           audio.reload();
@@ -49,6 +53,13 @@ document.addEventListener('DOMContentLoaded', () => {
         if (audio.playing) {
           audio.masterGain.gain.value = 0;
           audio.stop();
+          clickElement.object.material.color.set(0x000000);
+          world.scene.children[3].children.filter(obj => (
+            obj.name === 'pause'
+          ))[0].material.color.set(0xffffff);
+          world.scene.children[3].children.filter(obj => (
+            obj.name === 'play'
+          ))[0].material.color.set(0xffffff);
         }
         audio.masterGain.gain.value = 1;
         window.removeEventListener('mouseup', handleClick, false);
@@ -59,36 +70,46 @@ document.addEventListener('DOMContentLoaded', () => {
       case 'mute':
         if (audio.masterGain.gain.value) {
           audio.masterGain.gain.value = 0;
+          clickElement.object.material.color.set(0x000000);
         } else {
           audio.masterGain.gain.value = 1;
+          clickElement.object.material.color.set(0xffffff);
         }
         break;
       case 'track1':
         if (audio.drumsGain.gain.value) {
           audio.drumsGain.gain.value = 0;
+          clickElement.object.material.color.set(0x000000);
         } else {
           audio.drumsGain.gain.value = 1;
+          clickElement.object.material.color.set(0xffffff);
         }
         break;
       case 'track2':
         if (audio.bassGain.gain.value) {
           audio.bassGain.gain.value = 0;
+          clickElement.object.material.color.set(0x000000);
         } else {
           audio.bassGain.gain.value = 1;
+          clickElement.object.material.color.set(0xffffff);
         }
         break;
       case 'track3':
         if (audio.melodyGain.gain.value) {
           audio.melodyGain.gain.value = 0;
+          clickElement.object.material.color.set(0x000000);
         } else {
           audio.melodyGain.gain.value = 1;
+          clickElement.object.material.color.set(0xffffff);
         }
         break;
       case 'track4':
         if (audio.samplesGain.gain.value) {
           audio.samplesGain.gain.value = 0;
+          clickElement.object.material.color.set(0x000000);
         } else {
           audio.samplesGain.gain.value = 1;
+          clickElement.object.material.color.set(0xffffff);
         }
         break;
     }
